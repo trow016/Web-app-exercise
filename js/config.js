a5c3ccb5d76f0add143fe42c160e3912261e9f81
trow016 <54686503+trow016@users.noreply.github.com>
2021-04-05 12:46:23 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_NHM2B0c8z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '202tcvo9rmgo1qudb4d0p66gqv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_kkN9W5r3w', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5v5ge3f73gkd1e70htf5pmve8u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
